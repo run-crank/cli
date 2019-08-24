2f6947ddc5ea21fe83f7762713944c4359fcf447
Eric Peterson <i.am@eric.pe>
2019-08-23 19:10:06 -0700

    First stab at useful tests.

--- test/commands/cog-install.test.ts
+++ test/commands/cog-install.test.ts
@@ -2,34 +2,23 @@ import {expect, test} from '@oclif/test'
 import { sync as rmRfSync } from 'rimraf';
 
 describe('cog:install', () => {
-  const cacheDir: string = `${__dirname}/../../../.test_cache`;
+  const cacheDir: string = `${__dirname}/../.test_cache`;
 
   beforeEach(() => {
     process.env.CRANK_CACHEDIR = cacheDir;
     rmRfSync(cacheDir);
+    require('child_process').spawnSync('mkdir', [cacheDir])
   })
 
-  /*test
-  .stdout()
-  .command(['cog:install', '--source=local', '--local-start-command="crank cog:meta"', '--ignore-auth'])
-  .it('shows user email when logged in', ctx => {
-    expect(ctx.stdout).to.equal('Successfully installed MetaCog cog.\n')
-  })*/
-
-  /*test
-  .nock('https://api.heroku.com', api => api
-    .get('/account')
-    // HTTP 401 means the user is not logged in with valid credentials
-    .reply(401)
-  )
-  .command(['auth:whoami'])
-  // checks to ensure the command exits with status 100
-  .exit(100)
-  .it('exits with status 100 when not logged in')*/
+  test
+    .stdout()
+    .command(['cog:install', '--source', 'local', '--local-start-command', './bin/run cog:meta', '--ignore-auth'])
+    .it('can install local cog', ctx => {
+      expect(ctx.stdout).to.equal('Successfully installed automatoninc/metacog cog.\n')
+    })
 
   afterEach(() => {
     delete process.env.CRANK_CACHEDIR;
-    console.log('yup');
   });
 
 });
