f732ccd08e664e2673a670eb7cc707bc4707aa50
Eric Peterson <iamEAP@users.noreply.github.com>
2020-01-08 15:53:41 -0800

    Fix lint errors on fresh cog:scaffold.

--- src/assets/templates/typescript/src/client/mixins/user-aware.ts
+++ src/assets/templates/typescript/src/client/mixins/user-aware.ts
@@ -2,15 +2,15 @@
 import * as needle from 'needle';
 
 export class UserAwareMixin {
-    client: any;
+  client: any;
 
-    /**
-     * An example of how to expose the underlying API client to your steps. Any
-     * public methods exposed on this class can be invoked in your steps'
-     * executeStep methods like so: this.client.getUserByEmail()
-     */
-    public async getUserByEmail(email: string): Promise<needle.NeedleResponse> {
-      // Naturally, the code here will depend on the actual API client you use.
-      return this.client(`https://jsonplaceholder.typicode.com/users?email=${email}`);
-    }
+  /**
+   * An example of how to expose the underlying API client to your steps. Any
+   * public methods exposed on this class can be invoked in your steps'
+   * executeStep methods like so: this.client.getUserByEmail()
+   */
+  public async getUserByEmail(email: string): Promise<needle.NeedleResponse> {
+    // Naturally, the code here will depend on the actual API client you use.
+    return this.client(`https://jsonplaceholder.typicode.com/users?email=${email}`);
+  }
 }
