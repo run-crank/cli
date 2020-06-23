579592c3fd37edc2703ab93003c974beec072d77
Eric Peterson <i.am@eric.pe>
2020-06-23 07:55:52 -0700

    Add test automation for {{date(*)}} tokens #63

--- test/commands/run.test.ts
+++ test/commands/run.test.ts
@@ -1,11 +1,15 @@
 import * as fs from 'fs'
+import * as moment from 'moment';
 import {expect, test} from '@oclif/test'
 import { sync as rmRfSync } from 'rimraf';
 
+const chrono = require('chrono-node');
 const cacheDir: string = `${__dirname}/../.test_cache`;
 const scenarioDir: string = `${__dirname}/../fixtures/scenarios`;
 
 describe('run', () => {
+
+  let dateTokenValues: Record<string, moment.Moment> = {};
 
   before(() => {
     process.env.CRANK_CACHEDIR = cacheDir;
@@ -14,6 +18,7 @@ describe('run', () => {
   });
 
   beforeEach(() => {
+    dateTokenValues = {};
     fs.writeFileSync(`${cacheDir}/default-profile.json`, JSON.stringify([{
       cog: 'automatoninc/metacog',
       auth: {
@@ -125,6 +130,38 @@ describe('run', () => {
       expect(ctx.stdout).to.matches(/✓ Then the text Zounds! should equal Zounds!.*✓ Then the text Zounds! should equal Zounds!/is);
       expect(ctx.stdout).to.contain('2 passing');
       expect(ctx.stdout).to.not.contain('skipped');
+    });
+
+  test
+    .stdout()
+    .stderr()
+    .do(() => {
+      // It is possible for this test to fail in instances when this step starts
+      // just before an hour/day change, and command execution begins just after.
+      // ¯\_(ツ)_/¯
+      const anchor = new Date();
+      dateTokenValues.hourAgo = moment(chrono.parseDate('1 hour ago', anchor));
+      dateTokenValues.hourFromNow = moment(chrono.parseDate('1 hour from now', anchor));
+      dateTokenValues.dayAgo = moment(chrono.parseDate('1 day ago', anchor));
+      dateTokenValues.dayFromNow = moment(chrono.parseDate('1 day from now', anchor));
+      dateTokenValues.today = moment(chrono.parseDate('today', anchor));
+      dateTokenValues.midnightYesterdayUtc = moment(chrono.parseDate('midnight yesterday utc', anchor));
+      dateTokenValues.midnight2DaysAgoUtc = moment(chrono.parseDate('midnight 2 days ago utc', anchor));
+      dateTokenValues.midnightTomorrowUtc = moment(chrono.parseDate('midnight tomorrow utc', anchor));
+      dateTokenValues.midnight2DaysFromNowUtc = moment(chrono.parseDate('midnight 2 days from now utc', anchor));
+    })
+    .command(['run', `${scenarioDir}/uses-date-token.crank.yml`])
+    .exit(1)
+    .it('can use date tokens', ctx => {
+      expect(ctx.stdout).to.match(new RegExp(`1 hour ago is ${dateTokenValues.hourAgo.utc().format('YYYY-MM-DD[T]HH')}`));
+      expect(ctx.stdout).to.match(new RegExp(`1 hour from now is ${dateTokenValues.hourFromNow.utc().format('YYYY-MM-DD[T]HH')}`));
+      expect(ctx.stdout).to.match(new RegExp(`1 day ago is ${dateTokenValues.dayAgo.utc().format('YYYY-MM-DD')}`));
+      expect(ctx.stdout).to.match(new RegExp(`1 day from now is ${dateTokenValues.dayFromNow.utc().format('YYYY-MM-DD')}`));
+      expect(ctx.stdout).to.match(new RegExp(`today is ${dateTokenValues.today.utc().format('YYYY-MM-DD')}`));
+      expect(ctx.stdout).to.match(new RegExp(`m-y is ${dateTokenValues.midnightYesterdayUtc.utc().format('YYYY-MM-DD[T]HH:mm:ss')}`));
+      expect(ctx.stdout).to.match(new RegExp(`m-2 is ${dateTokenValues.midnight2DaysAgoUtc.utc().format('YYYY-MM-DD[T]HH:mm:ss')}`));
+      expect(ctx.stdout).to.match(new RegExp(`m-t is ${dateTokenValues.midnightTomorrowUtc.utc().format('YYYY-MM-DD[T]HH:mm:ss')}`));
+      expect(ctx.stdout).to.match(new RegExp(`mx2 is ${dateTokenValues.midnight2DaysFromNowUtc.utc().format('YYYY-MM-DD[T]HH:mm:ss')}`));
     });
 
   test
